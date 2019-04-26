63df64657fc1ecf2c03ad2c8a6a0eaae51929ed1
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-04-26 14:28:09 +0900

    Update indent

--- CommandProcess/CommandProcess.swift
+++ CommandProcess/CommandProcess.swift
@@ -12,55 +12,55 @@ import Foundation
 //
 // https://developer.apple.com/documentation/servicemanagement/1431078-smjobbless
 public class CommandProcess {
-    public struct Result {
-        var outputMessage: String
-        var errorMessage: String
-        
-        var hasError: Bool {
-            if errorMessage.count > 0 {
-                return true
-            }
-            
-            return false
-        }
+  public struct Result {
+    var outputMessage: String
+    var errorMessage: String
+  
+    var hasError: Bool {
+      if errorMessage.count > 0 {
+        return true
+      }
+      
+      return false
     }
+  }
 }
 
 public extension CommandProcess {
-    private static let shellPath = "/bin/sh"
-    
-    static func executeShell(_ args: [String]) -> Result {
-        return execute(shellPath, args: args)
+  private static let shellPath = "/bin/sh"
+  
+  static func executeShell(_ args: [String]) -> Result {
+    return execute(shellPath, args: args)
+  }
+  
+  static func execute(_ path: String, args: [String]) -> Result {
+    let process = Process.init()
+    let outputPipe = Pipe.init()
+    let errorPipe = Pipe.init()
+  
+    process.launchPath = path
+    process.arguments = args
+    process.standardOutput = outputPipe
+    process.standardError = errorPipe
+  
+  
+    process.launch()
+    process.waitUntilExit()
+  
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    var outputMessage = ""
+    if(outputData.count > 0) {
+      outputMessage = String(data: outputData, encoding: .utf8) ?? ""
     }
     
-    static func execute(_ path: String, args: [String]) -> Result {
-        let process = Process.init()
-        let outputPipe = Pipe.init()
-        let errorPipe = Pipe.init()
-        
-        process.launchPath = path
-        process.arguments = args
-        process.standardOutput = outputPipe
-        process.standardError = errorPipe
-        
-        
-        process.launch()
-        process.waitUntilExit()
-        
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        var outputMessage = ""
-        if(outputData.count > 0) {
-            outputMessage = String(data: outputData, encoding: .utf8) ?? ""
-        }
-        
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        var errorMessage = ""
-        if(errorData.count > 0) {
-            errorMessage = String(data: errorData, encoding: .utf8) ?? ""
-        }
-        
-        let res = Result(outputMessage: outputMessage, errorMessage: errorMessage)
-        
-        return res
+    let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+    var errorMessage = ""
+    if(errorData.count > 0) {
+      errorMessage = String(data: errorData, encoding: .utf8) ?? ""
     }
+  
+    let res = Result(outputMessage: outputMessage, errorMessage: errorMessage)
+    
+    return res
+  }
 }
--- CommandProcessTests/CommandProcessTests.swift
+++ CommandProcessTests/CommandProcessTests.swift
@@ -11,75 +11,75 @@ import XCTest
 
 class CommandProcessTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testShellVersion() {
+    let result = CommandProcess.executeShell(["-c", "sh --version"])
+    print(result)
     
-    func testShellVersion() {
-        let result = CommandProcess.executeShell(["-c", "sh --version"])
-        print(result)
-        
-        XCTAssert(!result.hasError)
-        XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
-    }
+    XCTAssert(!result.hasError)
+    XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
+  }
+  
+  func testEcho() {
+    let string = "Hello World"
+    let result = CommandProcess.executeShell(["-c", "echo '\(string)'"])
+    print(result)
     
-    func testEcho() {
-        let string = "Hello World"
-        let result = CommandProcess.executeShell(["-c", "echo '\(string)'"])
-        print(result)
-        
-        XCTAssert(!result.hasError)
-        XCTAssertEqual(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines), string)
-    }
+    XCTAssert(!result.hasError)
+    XCTAssertEqual(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines), string)
+  }
 
-    func testLS() {
-        let result = CommandProcess.executeShell(["-c", "ls"])
-        print(result)
-        
-        XCTAssert(!result.hasError)
-        XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
-    }
+  func testLS() {
+    let result = CommandProcess.executeShell(["-c", "ls"])
+    print(result)
+    
+    XCTAssert(!result.hasError)
+    XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
+  }
 
-    func testPWD() {
-        let result = CommandProcess.executeShell(["-c", "pwd"])
-        print(result)
-        
-        XCTAssert(!result.hasError)
-        XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
-    }
+  func testPWD() {
+    let result = CommandProcess.executeShell(["-c", "pwd"])
+    print(result)
     
-    func testCommandError() {
-        let result = CommandProcess.executeShell(["-c", "ls /nonexist"])
-        print(result)
-        
-        XCTAssert(result.hasError)
-    }
+    XCTAssert(!result.hasError)
+    XCTAssert(result.outputMessage.trimmingCharacters(in: .whitespacesAndNewlines).count > 0)
+  }
+  
+  func testCommandError() {
+    let result = CommandProcess.executeShell(["-c", "ls /nonexist"])
+    print(result)
     
-    func testCommandNotFoundError() {
-        let result = CommandProcess.executeShell(["-c", "invalidcommand"])
-        print(result)
-        
-        XCTAssert(result.hasError)
-    }
+    XCTAssert(result.hasError)
+  }
+  
+  func testCommandNotFoundError() {
+    let result = CommandProcess.executeShell(["-c", "invalidcommand"])
+    print(result)
     
-    func testPermissionError() {
-        let result = CommandProcess.executeShell(["-c", "purge"])
-        print(result)
-        
-        XCTAssert(result.hasError)
-    }
+    XCTAssert(result.hasError)
+  }
+  
+  func testPermissionError() {
+    let result = CommandProcess.executeShell(["-c", "purge"])
+    print(result)
     
-    /*
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    XCTAssert(result.hasError)
+  }
+  
+  /*
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    */
+  }
+  */
 
 }
